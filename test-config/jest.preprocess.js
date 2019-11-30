01947b4d2eee835b5a823be3215ed94fbfd49ab9
Ernie Bello <ernie@bello.nyc>
2019-11-30 09:22:28 -0500

    editor config, netlify config, circleci config

--- test-config/jest.preprocess.js
+++ test-config/jest.preprocess.js
@@ -1,9 +1,6 @@
 const babelOptions = {
   presets: ['babel-preset-gatsby', '@emotion/babel-preset-css-prop'],
-  // plugins: [
-  //   ['@emotion/babel-preset-css-prop'],
-  // ],
 };
 
- // eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line import/no-extraneous-dependencies
 module.exports = require('babel-jest').createTransformer(babelOptions);
